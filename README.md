9d0af4302744a2c6c4afe5c0da5f6b06f658e65d
mdurrant <mdurrant@expressmedia.com>
2019-11-26 09:10:24 -0700

    add email/avatar/displayname support

--- README.md
+++ README.md
@@ -16,6 +16,11 @@ You can then sign tokens for your users like so:
 require __DIR__ . '/vendor/autoload.php';
 
 $sponsor = new NitroPay\Sponsor('YOUR_PRIVATE_KEY');
+
+$sponsor->setEmail('example@example.com'); // optional: pass along the user's email to pre-fill in the form
+$sponsor->setAvatar('https://s.gravatar.com/avatar/0d3964876826ac9554d88d5a51ea87a2?s=80'); // optional: avatar
+$sponsor->setDisplayName('John Doe'); // optional: display name, user will be identified by their ID if this isn't set
+
 $token = $sponsor->sign(109, 39281); // in this example, site ID = 109, user ID = 39281
 ```
 
--- src/Sponsor.php
+++ src/Sponsor.php
@@ -9,6 +9,9 @@ use Lcobucci\JWT\Signer\Hmac\Sha512;
 class Sponsor
 {
     protected $privateKey;
+    protected $email;
+    protected $avatar;
+    protected $displayName;
 
     public function __construct(string $privateKey) {
         if (!$privateKey) {
@@ -16,16 +19,37 @@ class Sponsor
         }
 
         $this->privateKey = $privateKey;
+        $this->email = '';
+        $this->avatar = '';
+    }
+
+    public function setEmail(string $email) {
+        $this->email = $email;
+    }
+
+    public function setAvatar(string $avatar) {
+        $this->avatar = $avatar;
+    }
+
+    public function setDisplayName(string $displayName) {
+        $this->avatar = $avatar;
     }
 
     public function sign($siteId, $userId) {
         $signer = new Sha512();
         $time = time();
 
+        if (!$this->displayName) {
+            $this->displayName = $userId;
+        }
+
         $token = (new Builder())->issuedBy((string) $siteId)
                                 ->setSubject((string) $userId)
                                 ->issuedAt($time)
                                 ->canOnlyBeUsedAfter($time)
+                                ->withClaim('displayName', $this->displayName)
+                                ->withClaim('email', $this->email)
+                                ->withClaim('avatar', $this->avatar)
                                 ->getToken($signer, new Key($this->privateKey));
 
         return $token;
